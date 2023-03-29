ff5fbb707a75e4cab852a069909b5f767b8ef6c0
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-03-29 22:40:44 +0200

    Add validations to user and post models

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,15 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  # Title must not be blank.
+  validates :title, presence: true, length: { maximum: 250 }
+
+  # CommentsCounter must be an integer greater than or equal to zero.
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  # LikesCounter must be an integer greater than or equal to zero.
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   after_save :update_post_counter
 
   def update_post_counter
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,12 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
 
+  # Name must not be blank.
+  validates :name, presence: true
+  
+  # PostsCounter must be an integer greater than or equal to zero.
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def most_recent_posts
     posts.limit(3).order(created_at: :desc)
   end
