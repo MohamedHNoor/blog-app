a09073661c756f0e33c286068f863476eb37eb4c
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-03-29 23:14:25 +0200

    Fix rubocop error

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
 
   # Name must not be blank.
   validates :name, presence: true
-  
+
   # PostsCounter must be an integer greater than or equal to zero.
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
