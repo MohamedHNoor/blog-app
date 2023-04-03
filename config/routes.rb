97e658343bf7e187a6fe1f30377cdc3954646035
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-04-03 10:59:21 +0200

    Add posts routes and controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "users#index"
-  get "/users/:id", to: "users#show"
+  get "/users/:id/", to: "users#show"
+
+  get "users/:user_id/posts", to: "posts#index"
+  get "users/:user_id/posts/:id/", to: "posts#show"
 
 end
