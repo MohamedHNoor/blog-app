f0daea298fbc562803cadd12f37b1270cb591de7
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-04-04 05:38:10 +0200

    Fix rubocop errors

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,31 +2,31 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
-    before(:example) { get '/users/:user_id/posts'}
+    before(:example) { get '/users/:user_id/posts' }
     it 'returns http success' do
       expect(response).to have_http_status(:ok)
     end
 
-    it ('is correct template rendered') do
+    it('is correct template rendered') do
       expect(response).to render_template(:index)
     end
 
-    it ('is response body includes correct placeholder text') do
+    it('is response body includes correct placeholder text') do
       expect(response.body).to include('index method')
     end
   end
 
   describe 'GET /show' do
-    before (:example) {get '/users/:user_id/posts/:post_id'}
+    before(:example) { get '/users/:user_id/posts/:post_id' }
     it 'returns http success' do
       expect(response).to have_http_status(:ok)
     end
 
-    it ('is correct template rendered') do
+    it('is correct template rendered') do
       expect(response).to render_template(:show)
     end
 
-    it ('is response body includes correct placeholder text') do
+    it('is response body includes correct placeholder text') do
       expect(response.body).to include('show method')
     end
   end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -9,27 +9,27 @@ RSpec.describe 'Users', type: :request do
       expect(response).to have_http_status(:ok)
     end
 
-    it ('is correct template rendered') do
+    it('is correct template rendered') do
       expect(response).to render_template(:index)
     end
 
-    it ('is response body includes correct placeholder text') do
+    it('is response body includes correct placeholder text') do
       expect(response.body).to include('List of users in index method')
     end
   end
 
   describe 'GET /show' do
-    before (:example) {get '/users/:user_id'}
-    
+    before(:example) { get '/users/:user_id' }
+
     it 'returns http success' do
       expect(response).to have_http_status(:ok)
     end
 
-    it ('is correct template rendered') do
+    it('is correct template rendered') do
       expect(response).to render_template(:show)
     end
 
-    it ('is response body includes correct placeholder text') do
+    it('is response body includes correct placeholder text') do
       expect(response.body).to include('Single user in show method')
     end
   end
