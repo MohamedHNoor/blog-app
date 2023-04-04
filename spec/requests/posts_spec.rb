5a21f16dab25014d4c057da54fb49b96e5b3def5
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-04-04 05:32:04 +0200

    Add rspec test for post controller

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,16 +2,32 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
+    before(:example) { get '/users/:user_id/posts'}
     it 'returns http success' do
-      get '/posts/index'
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:ok)
+    end
+
+    it ('is correct template rendered') do
+      expect(response).to render_template(:index)
+    end
+
+    it ('is response body includes correct placeholder text') do
+      expect(response.body).to include('index method')
     end
   end
 
   describe 'GET /show' do
+    before (:example) {get '/users/:user_id/posts/:post_id'}
     it 'returns http success' do
-      get '/posts/show'
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:ok)
+    end
+
+    it ('is correct template rendered') do
+      expect(response).to render_template(:show)
+    end
+
+    it ('is response body includes correct placeholder text') do
+      expect(response.body).to include('show method')
     end
   end
 end
