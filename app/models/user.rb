1a65e1aa18caacb3ca6c4e0f84a82e21a2405f60
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-04-05 10:51:59 +0200

    Update user and post models

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: 'author_id'
 
   validates :name, presence: true
-  # validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def most_recent_posts
     posts.last(3)
