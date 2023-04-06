f3514a861aa52aee31d8322b7843194ce85bc3bb
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-04-06 17:50:05 +0200

    Add comments_controller for create method

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,15 @@
+class CommentsController < ApplicationController
+
+  def create
+    @comment = current_user.comments.new(comment_params)
+    @comment.post_id = params[:post_id]
+    @comment.save
+    flash[:success] = "Commented!"
+    redirect_to "/users/#{current_user.id}/posts"
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+end
