6bb9f81d8f9af29d415a82aae66104c0a6d673b8
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-04-06 17:49:17 +0200

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "users#index"
+
+  get 'posts/new', to: 'posts#new'
+  post 'posts', to: 'posts#create'
+  post 'likes/:post_id', to: 'likes#create'
+  post 'comments/:post_id', to: 'comments#create'
   
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
