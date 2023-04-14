dc0aa552c1b496cbf9ba6d3cfa4f5ccc47d49ce2
Christian Ani <iamchristianani@gmail.com>
2023-04-14 11:16:52 +0100

    Create posts controller for the API

---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,16 @@
+require 'jwt'
+class Api::V1::PostsController < Api::V1::ApplicationController
+  before_action :find_user_prams
+  def index
+    if @user
+      @posts = @user.posts.order(created_at: :asc)
+      render json: @posts
+    else
+      render json: { status: 'Failure', error: 'Post Not Found' }
+    end
+  end
+  private
+  def find_user_prams
+    @user = User.find(params[:user_id])
+  end
+end
